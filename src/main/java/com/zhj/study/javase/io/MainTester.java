f8e1f725b00eecbdd99b731d0907c80f5415a525
shinsanzhj <mindofzhj@163.com>
2016-11-15 16:58:50 +0800

    添加IO测试类

---
+++ src/main/java/com/zhj/study/javase/io/MainTester.java
@@ -0,0 +1,17 @@
+package com.zhj.study.javase.io;
+
+public class MainTester {
+
+	/**
+	 * @param args
+	 *
+	 * <pre>
+	 * 修改日期		修改人	修改原因
+	 * 2016-11-15	钟华杰	新建
+	 * </pre>
+	 */
+	public static void main(String[] args) {
+		System.out.println("用来测试IO");
+	}
+
+}
